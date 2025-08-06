e0e013e939155b083b286cd3e639bdcc19646eb3
philouLeF <delcroixphilippe73@gmail.com>
2025-08-06 17:39:55 +0200

    début du style des cards

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { getAllCountries } from "../services/countriesService";
+import { css } from "../../styled-system/css";
 
 export const Route = createFileRoute("/")({
   component: HomePage,
@@ -9,23 +10,58 @@ function HomePage() {
   const countries = getAllCountries();
 
   return (
-    <div>
+    <div
+      className={css({
+        maxWidth: "90%",
+        margin: "0 auto",
+      })}
+    >
       <h1>liste pays</h1>
-      <ul>
+      <ul
+        className={css({
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "12",
+        })}
+      >
         {countries.map((country) => (
-          <li key={country.alpha3Code}>
-            <Link
-              to="/country/$countryId"
-              params={{ countryId: country.alpha3Code.toLowerCase() }}
+          <Link
+            className={css({
+              maxWidth: { base: "90%", md: "20%" },
+              borderRadius: "md",
+              overflow: "hidden",
+              boxShadow: "md",
+              transition: "all 0.3s ease",
+              "&:hover": {
+                transform: "scale(1.05)",
+              },
+            })}
+            key={country.alpha3Code}
+            to="/country/$countryId"
+            params={{ countryId: country.alpha3Code.toLowerCase() }}
+          >
+            <img
+              className={css({
+                width: "100%",
+                height: "50%",
+                objectFit: "cover",
+              })}
+              src={country.flags.svg}
+              alt={`${country.name} flags`}
+            />
+            <div
+              className={css({
+                padding: "4",
+              })}
             >
-              <img src={country.flags.svg} alt={`${country.name} flags`} />
-              <h3>{country.name}</h3>
+              <h3 className={css({ fontWeight: "bold", py: "4" })}>
+                {country.name}
+              </h3>
               <p>Population: {country.population.toLocaleString()}</p>
               <p>Region: {country.region}</p>
               <p>Capital: {country.capital}</p>
-              <p>Code: {country.alpha3Code}</p>
-            </Link>
-          </li>
+            </div>
+          </Link>
         ))}
       </ul>
     </div>
